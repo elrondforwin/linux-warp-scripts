8c4e39b5877b74e88825fe3e921052699a59d433
MustafaBack <aydinmustafaarda1@gmail.com>
2025-07-24 20:13:53 +0300

    Update warp-dpi-bypass-python.py

--- experimental/warp-dpi-bypass-python.py
+++ experimental/warp-dpi-bypass-python.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env
+# set -e
+
 import subprocess
 import time
 import shutil
@@ -9,29 +12,28 @@ def run_command(command, require_sudo=False):
             command.insert(0, 'sudo')
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError:
-        print(f"HATA: Komut başarısız: {' '.join(command)}")
+        print(f"ERROR: Command failed: {' '.join(command)}")
         sys.exit(1)
-
-# 1. Cloudflare WARP yüklü mü kontrol et
+        
+# Check if Cloudflare WARP is installed
 print("Check if Cloudflare WARP is installed")
 if shutil.which("warp-cli") is None:
     print("Cloudflare WARP is not installed. Please install it and try later.")
     sys.exit(1)
 
-# 2. systemd-resolved durdur ve maskelen
+# [1/7] stopping and disabling systemd-resolved
 print("[1/7] stopping and disabling systemd-resolved")
 run_command(["systemctl", "disable", "--now", "systemd-resolved"], require_sudo=True)
 run_command(["systemctl", "mask", "systemd-resolved"], require_sudo=True)
 
-# 3. warp-svc servisini etkinleştir ve başlat
+# [2/7] activating and initializing warp-svc
 print("[2/7] activating and initializing warp-svc")
 run_command(["systemctl", "enable", "--now", "warp-svc"], require_sudo=True)
 
-# 4. 5 saniye bekle
+# [3/7] waiting 5 seconds
 print("[3/7] waiting 5 seconds")
 time.sleep(5)
 
-# 5. warp bağlantısını kur
+# [4/7] establishing a warp connection...
 print("[4/7] establishing a warp connection...")
 run_command(["warp-cli", "connect"], require_sudo=True)
-
