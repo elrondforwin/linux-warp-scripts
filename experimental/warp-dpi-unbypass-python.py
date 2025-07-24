5c18e27b97828b5bebc10118f2c4ec4a2dd0a8be
MustafaBack <aydinmustafaarda1@gmail.com>
2025-07-24 20:15:17 +0300

    Update warp-dpi-unbypass-python.py

--- experimental/warp-dpi-unbypass-python.py
+++ experimental/warp-dpi-unbypass-python.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env
+# set -e
+
 import subprocess
 import sys
 
@@ -7,19 +10,18 @@ def run_command(command, require_sudo=False):
             command.insert(0, 'sudo')
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError:
-        print(f"HATA: Komut başarısız: {' '.join(command)}")
+        print(f"ERROR: Command failed: {' '.join(command)}")
         sys.exit(1)
 
-# 1. Warp bağlantısını kes
+# [1/3] Warp disconnect...
 print("[1/3] Warp disconnect...")
 run_command(["warp-cli", "disconnect"], require_sudo=True)
 
-# 2. warp-svc servisini durdur ve devre dışı bırak
+# [2/3] disabling the warp-svc service
 print("[2/3] disabling the warp-svc service")
 run_command(["systemctl", "disable", "--now", "warp-svc"], require_sudo=True)
 
-# 3. systemd-resolved tekrar etkinleştir
+# [3/3] re-enabling systemd-resolved...
 print("[3/3] re-enabling systemd-resolved...")
 run_command(["systemctl", "unmask", "systemd-resolved"], require_sudo=True)
 run_command(["systemctl", "enable", "--now", "systemd-resolved"], require_sudo=True)
-
