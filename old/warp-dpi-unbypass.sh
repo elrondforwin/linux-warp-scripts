3ca83b48aed3e67d2781f425fd63102d52ab8f61
MustafaBack <aydinmustafaarda1@gmail.com>
2025-07-26 11:46:20 +0300

    Update warp-dpi-unbypass.sh

--- old/warp-dpi-unbypass.sh
+++ old/warp-dpi-unbypass.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
 
+echo "[1/3] Warp disconnect..."
+sudo warp-cli disconnect
+
+echo "[2/3] disabling the warp-svc service"
+sudo systemctl disable --now warp-svc
+
+echo "[3/3] re-enabling systemd-resolved..."
+sudo systemctl unmask systemd-resolved
+sudo systemctl enable --now systemd-resolved
